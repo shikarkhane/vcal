a172bba1f6a34ac3c43b934192aa5125912bb3d9
nikhil shikarkhane <nikhils@spotify.com>
2016-10-20 14:20:57 +0200

    demo flow

---
+++ static/js/demo-flow.js
@@ -0,0 +1,35 @@
+$(".act-home").click(function(){
+    window.location.replace("/");
+});
+$(".act-sign-in").click(function(){
+    window.location.replace("/");
+});
+
+$(".act-log-in").click(function(){
+    window.location.replace("/template/dashboard/");
+});
+$(".act-summon").click(function(){
+    window.location.replace("/template/summon/");
+});
+$(".act-work-day").click(function(){
+    window.location.replace("/template/work-day/");
+});
+$(".act-term").click(function(){
+    window.location.replace("/template/term/");
+});
+$(".act-show-ups").click(function(){
+    window.location.replace("/template/show-ups/");
+});
+$(".act-invite").click(function(){
+    window.location.replace("/template/invite/");
+});
+$(".act-members").click(function(){
+    window.location.replace("/template/members/");
+});
+$(".act-children").click(function(){
+    window.location.replace("/template/children/");
+});
+$(".act-work-sign-up").click(function(){
+    window.location.replace("/template/work-sign-up/");
+});
+
