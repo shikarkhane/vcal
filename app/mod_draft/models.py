29d8a2cef8e2441a05ab3481dac4931a85302ca6
Nikhilm <shikarkhane@gmail.com>
2016-11-17 17:04:19 +0100

    adding models

--- app/mod_draft/models.py
+++ app/mod_draft/models.py
@@ -15,26 +15,43 @@ class Base(db.Model):
 # Define a User model
 class User(Base):
 
-    __tablename__ = 'auth_user'
-
-    # User Name
-    name    = db.Column(db.String(128),  nullable=False)
+    __tablename__ = 'user'
 
     # Identification Data: email & password
     email    = db.Column(db.String(128),  nullable=False,
                                             unique=True)
-    password = db.Column(db.String(192),  nullable=False)
+    password = db.Column(db.String(192),  nullable=True)
+    auth_token = db.Column(db.String(300), nullable=True)
 
     # Authorisation Data: role & status
     role     = db.Column(db.SmallInteger, nullable=False)
     status   = db.Column(db.SmallInteger, nullable=False)
 
     # New instance instantiation procedure
-    def __init__(self, name, email, password):
+    def __init__(self, name, email, password, token):
 
         self.name     = name
         self.email    = email
         self.password = password
+        self.auth_token = token
 
     def __repr__(self):
         return '<User %r>' % (self.name)
+
+class PublicHoliday(Base):
+
+    __tablename__ = 'public_holiday'
+
+    # date
+    holiday_date    = db.Column(db.DateTime,  nullable=False)
+    # full day or half day
+    is_halfday   = db.Column(db.SmallInteger, nullable=False)
+
+    # New instance instantiation procedure
+    def __init__(self, date, is_halfday):
+
+        self.holiday_date  = date
+        self.is_halfday    = is_halfday
+
+    def __repr__(self):
+        return '<Date %r>' % (str(self.holiday_date))
