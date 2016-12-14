6dbda860e80827f1eabe91a6d8cd22863719ade6
nikhil shikarkhane <nikhils@spotify.com>
2016-12-14 15:05:12 +0100

    showups fixed

--- app/static/js/workday/showups.js
+++ app/static/js/workday/showups.js
@@ -1,13 +1,21 @@
 
 $("#confirm-showups").click(function(){
     work_date = $("#showup-chosen-date").val();
+    workday_user_ids = [];
+    standin_user_ids= [];
+    $( ".cls-workday-date" ).each(function( index ) {
+      workday_user_ids.push($( this ).attr("data-user-id"));
+    });
+    $( ".cls-standin-date" ).each(function( index ) {
+      standin_user_ids.push($( this ).attr("data-user-id"));
+    });
 
     $.ajax({
           contentType : 'application/json',
           method: "POST",
           url: "/sv/show-ups/",
-          data: JSON.stringify({ group_id: 1, work_date: work_date,
-          workday_user_ids: [1,2,3], standin_user_ids: [23]})
+          data: JSON.stringify({ group_id: 1, chosen_date: work_date,
+          workday_user_ids: workday_user_ids, standin_user_ids: standin_user_ids})
         })
           .done(function( msg ) {
             console.log('showup was saved');
