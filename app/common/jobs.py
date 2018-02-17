5cf3d4532589847f2f10928c79d10b882591ce99
Nikhilm <shikarkhane@gmail.com>
2018-02-17 21:01:03 +0100

    weekly reminder - user names pending

--- app/common/jobs.py
+++ app/common/jobs.py
@@ -1,5 +1,6 @@
 from app.mod_group.bl import getAllGroups, getGroupByDomain
-from app.mod_workday.bl import getOpenStandin, getOpenWorkday
+from app.mod_workday.bl import getOpenStandin, getOpenWorkday, \
+    getStandinVikarieForNextXDays, getWorkdayVikarieForNextXDays
 from app.mod_communicate.bl import Message
 from app.common.bl import getGroupAdmins
 from app.common.notify import notify_unbooked_to_admin
@@ -25,6 +26,24 @@ def unbooked_dates(event, context):
 
         fn = lambda x: DateUtil().getHumanDate(x)
         datesAsText = "Open Standins -  " + ",\n".join([fn(i) for i in os]) + "\n\n" + "Open Workdays -  " + ",\n".join([fn(i) for i in ow])
+
+        for admin in groupAdmins:
+            notify_unbooked_to_admin(admin['id'], datesAsText)
+    else:
+        logging.info("No admins exists")
+
+def weekly_reminder():
+    '''send reminder every friday evening to vikarie in upcoming week'''
+    #get group gomorronsol only
+    groupId = getGroupByDomain('gomorronsol.net')[0]["domain"]
+    groupAdmins = getGroupAdmins(groupId)
+
+    if not groupAdmins:
+        os = [[i['standin_date'], i['standin_user_id']] for i in getStandinVikarieForNextXDays(groupId,0,7)]
+        ow = [[i['work_date'], i['standin_user_id']] for i in getWorkdayVikarieForNextXDays(groupId,0,7)]
+
+        fn = lambda x: DateUtil().getHumanDate(x)
+        datesAsText = "Standins -  " + ",\n".join([fn(i[0]) for i in os]) + "\n\n" + "Workdays -  " + ",\n".join([fn(i[0]) for i in ow])
 
         for admin in groupAdmins:
             notify_unbooked_to_admin(admin['id'], datesAsText)
--- app/mod_workday/bl.py
+++ app/mod_workday/bl.py
@@ -1,5 +1,7 @@
 from app import engine
 from operator import itemgetter
+import datetime
+import calendar
 
 from app.mod_workday.models import Workday, StandinDay
 
@@ -11,6 +13,38 @@ def getOpenWorkday(group_id):
 def getOpenStandin(group_id):
     s = engine.query(StandinDay).filter(StandinDay.group_id == group_id,
                                         StandinDay.standin_user_id == None).all()
+    newS = sorted(s, key=itemgetter('standin_date'))
+    return newS
+
+def getWorkdayVikarieForNextXDays(group_id, start_after_x_days = 2, next_x_days = 5):
+    '''with default parameters, if job is executed on friday, it should return weekdays'''
+    # todo : make a utility to return next week start and end dates
+    date_start = datetime.datetime.now().date() + datetime.timedelta(days= start_after_x_days + 1)
+    date_end = date_start + datetime.timedelta(days=next_x_days + 1)
+
+    epoch_start = calendar.timegm(date_start.timetuple())
+    epoch_end = calendar.timegm(date_end.timetuple())
+
+    w = engine.query(Workday).filter(Workday.group_id == group_id,
+                                     Workday.standin_user_id != None,
+                                     Workday.work_date >= epoch_start,
+                                     Workday.work_date < epoch_end).all()
+    newS = sorted(w, key=itemgetter('work_date'))
+    return newS
+
+def getStandinVikarieForNextXDays(group_id, start_after_x_days = 2, next_x_days = 5):
+    '''with default parameters, if job is executed on friday, it should return weekdays'''
+    # todo : make a utility to return next week start and end dates
+    date_start = datetime.datetime.now().date() + datetime.timedelta(days= start_after_x_days + 1)
+    date_end = date_start + datetime.timedelta(days=next_x_days + 1)
+
+    epoch_start = calendar.timegm(date_start.timetuple())
+    epoch_end = calendar.timegm(date_end.timetuple())
+
+    s = engine.query(StandinDay).filter(StandinDay.group_id == group_id,
+                                        StandinDay.standin_user_id != None,
+                                        StandinDay.standin_date >= epoch_start,
+                                        StandinDay.standin_date < epoch_end).all()
     newS = sorted(s, key=itemgetter('standin_date'))
     return newS
 
